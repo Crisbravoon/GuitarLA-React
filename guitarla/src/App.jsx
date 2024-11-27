32cf9a2c37a3968bc31b1307eed4d0a0f3e0676a
Crisbravoon <cristobal_bravo6@hotmail.com>
2024-11-26 21:26:37 -0300

    Carrito terminado

--- guitarla/src/App.jsx
+++ guitarla/src/App.jsx
@@ -1,18 +1,84 @@
 
+import { useState } from 'react'
 import Guitar from './components/Guitar'
 import Header from './components/Header'
 import './index.css'
+import { db } from './data/GuitarDB'
 
 function App() {
 
+  const [data, setData] = useState(db)
+  const [cart, setCart] = useState([]);
+
+  const AddToCart = (guitar) => {
+
+    // Buscamos la existencia de la guitarra por id y que no se repita
+    const existingItem = cart.find(g => g.id === guitar.id);
+
+    // Si el item ya está en el carrito, incrementamos la cantidad,
+    if (existingItem) {
+      const updateCart = cart.map(g => g.id === guitar.id ? { ...g, quantity: g.quantity + 1 } : g);
+      setCart(updateCart);
+
+      // sino, agregamos el nuevo item al carrito con una cantidad.
+    } else {
+      setCart([...cart, { ...guitar, quantity: 1 }]);
+    }
+  };
+
+  const removeFromCart = (id) => {
+    const deleteCart = cart.filter(g => g.id !== id);
+    setCart(deleteCart);
+  };
+
+  const incrementQuantity = (id) => {
+    // Buscamos el item en el carrito por id y incrementamos la cantidad.
+    const updateCart = cart.map(item => {
+      if (item.id === id && item.quantity < 5) {
+        return { ...item, quantity: item.quantity + 1 };
+      }
+      return item;
+    })
+    setCart(updateCart);
+  };
+
+  const decrementQuantity = (id) => {
+    // Buscamos el item en el carrito por id y decrementamos la cantidad.
+    const updateCart = cart.map(item => {
+      if (item.id === id && item.quantity > 0) {
+        return { ...item, quantity: item.quantity - 1 }
+      }
+      return item;
+    })
+    setCart(updateCart);
+  };
+
+  const ClearCart = (e) => {
+    setCart([]);
+  };
+
   return (
     <>
-      <Header />
+      <Header
+        cart={cart}
+        removeFromCart={removeFromCart}
+        incrementQuantity={incrementQuantity}
+        decrementQuantity={decrementQuantity}               
+        ClearCart={ClearCart}/>
+
       <main className="container-xl mt-5">
         <h2 className="text-center">Nuestra Colección</h2>
 
         <div className="row mt-5">
-          <Guitar />
+          {data.map((guitar) => (
+
+            <Guitar key={guitar.id}
+              guitar={guitar}
+              setCart={setCart}
+              AddToCart={AddToCart}
+
+            />
+          ))}
         </div>
       </main>
 
--- guitarla/src/components/Guitar.jsx
+++ guitarla/src/components/Guitar.jsx
@@ -1,49 +1,26 @@
 
-import { useState } from 'react'
 
-import { db } from '../data/GuitarDB'
+const Guitar = ({ guitar, AddToCart }) => {
 
-const Guitar = () => {
-
-    const [data, setData] = useState(db);
-    const [cart, setCart] = useState([]);
-
-    const AddToCart = (guitar) => {
-
-        // Buscamos la existencia de la guitarra por id y que no se repita
-        const existingItem = cart.find(g => g.id === guitar.id);
-
-        // Si el item ya está en el carrito, incrementamos la cantidad,
-        if (existingItem) {
-            const updateCart = cart.map(g => 
-                g.id === guitar.id ? { ...g, guantity: g.guantity + 1 } : g);
-            setCart(updateCart);
-
-        // sino, agregamos el nuevo item al carrito con una cantidad.
-        }else{
-            setCart([...cart, {...guitar, guantity: 1 }]);
-        }
-    };
+    const { id, name, image, description, price } = guitar
 
     return (
         <>
-            {data.map((g) => (
-                <div className="col-md-6 col-lg-4 my-4 row align-items-center" key={g.id}>
-                    <div className="col-4" >
-                        <img className="img-fluid" src={`/img/${g.image}.jpg`} alt="imagen guitarra" />
-                    </div>
-                    <div className="col-8">
-                        <h3 className="text-black fs-4 fw-bold text-uppercase">{g.name}</h3>
-                        <p>{g.description}</p>
-                        <p className="fw-black text-primary fs-3">${g.price}</p>
-                        <button
-                            className="btn btn-dark w-100"
-                            onClick={() => AddToCart(g)}
-                            type="button"
-                        >Agregar al Carrito</button>
-                    </div>
+            <div className="col-md-6 col-lg-4 my-4 row align-items-center" key={id}>
+                <div className="col-4" >
+                    <img className="img-fluid" src={`/img/${image}.jpg`} alt="imagen guitarra" />
                 </div>
-            ))}
+                <div className="col-8">
+                    <h3 className="text-black fs-4 fw-bold text-uppercase">{name}</h3>
+                    <p>{description}</p>
+                    <p className="fw-black text-primary fs-3">${price}</p>
+                    <button
+                        className="btn btn-dark w-100"
+                        onClick={() => AddToCart(guitar)}
+                        type="button"
+                    >Agregar al Carrito</button>
+                </div>
+            </div>
         </>
     )
 }
